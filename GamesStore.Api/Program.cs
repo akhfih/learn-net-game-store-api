3aa0b5f5b3e6d16359e4e535c455c2cc8c68f520
Akhmad Fikron Huda <akhmad.fikron.huda@gmail.com>
2024-03-12 11:04:16 +0700

    applying migraions on startup

--- GamesStore.Api/Program.cs
+++ GamesStore.Api/Program.cs
@@ -1,6 +1,7 @@
 using GamesStore.Api.Data;
 using GamesStore.Api.Endpoints;
 using GamesStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
@@ -8,6 +9,8 @@ builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 var ConnString = builder.Configuration.GetConnectionString("GameStoreContext");
 builder.Services.AddSqlServer<GameStoreContext>(ConnString);
 var app = builder.Build();
+
+app.Services.InitializeDb();
 
 app.MapGamesEndpoints();
 
